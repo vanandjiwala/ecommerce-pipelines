fa83b03d0657ce30ed3785bd10ccb781d9149a0b
vanandjiwala <vasav.anandjiwala@gmail.com>
2026-01-28 13:20:44 +0000

    initial commit

---
+++ tests/test_sample.py
@@ -0,0 +1,7 @@
+from utils import dwh_utils
+import pytest
+
+
+def test_text_is_my_name_is_sss():
+    text = dwh_utils.print_name()
+    assert text == "my name is sss"
---
+++ utils/dwh_utils.py
@@ -0,0 +1,2 @@
+def print_name():
+    return "my name is sss"
